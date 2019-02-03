6d13183c5f7b164115fba05861df10b4f715d454
Aurore <aurorem@bam.tech>
2019-02-03 19:49:25 +0100

    Add master version of the app

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const os = require('os');
+
+console.log("App is running");
+
+var handler = function(request, response) {
+	response.writeHead(200);
+	response.end("You're on the master branch \n");
+};
+
+var www = http.createServer(handler);
+www.listen(8080);
