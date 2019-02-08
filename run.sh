8a03a2ef49083226c276605eff2a02e2ec6a2720
Yohan Gracia <yohang@padok.fr>
2019-02-08 15:19:02 +0100

    Everything is ok

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+#clean and copy files
 rm -rf ~/app/Dockerfile ~/app/app.js
-
 cp -a ~/tmp/Dockerfile ~/app
 cp -a ~/tmp/app.js ~/app
 rm -rf ~/tmp/Dockerfile ~/tmp/app.js
+cd app
 
 #Suppression de l'environnement et des ressources
 docker stop dumb-app-docker
