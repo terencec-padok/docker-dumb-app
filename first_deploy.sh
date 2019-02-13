633237236734e6f1ed90e063493e6b287220eac9
huntal <matthieu@lanvert.com>
2019-02-13 18:32:41 +0100

    a

--- first_deploy.sh
+++ first_deploy.sh
@@ -4,6 +4,6 @@
 source data.sh
 
 # We are going to setup EC2 instance ubuntu 18.04. Execute this script only if you got the same server (hosted on AWS).
-ssh -i $KEYNAME $SERVER 'bash -s' < setup.sh
+ssh -i $KEYNAME $SERVER 'bash -s' < clear.sh
 
 scp -i $KEYNAME ./app.js ./Dockerfile $SERVER:~/app
