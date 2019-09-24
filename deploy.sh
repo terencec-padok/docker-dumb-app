6b027146f43410df756769c96b765872c3196708
benjamins <benjamin@sanvoisin.io>
2019-09-24 10:26:30 +0000

    bash deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -eq 0 ] ; then
+    echo specify a branch
+    exit 0
+fi
+git checkout $1
+if [ $? -ne 0 ]; then
+    echo Branch doesnt exist
+    exit 0
+fi
+git pull
+docker build -t dumb-docker-app .
+docker stop docker-dumb-app
+if [ $? -eq 0 ]; then
+    echo Suppresion du container en prod
+    docker rm docker-dumb-app
+else
+    echo Pas de container en prod deployment en cours
+fi
+docker run -d -p 8080:8080 --name docker-dumb-app dumb-docker-app:latest
+echo container en production 
