9fa909212cbc1315bd1fcc153e35ba649d6f25d7
Philippe-Padok <philipped@padok.fr>
2020-10-08 15:48:56 +0200

    fix: update image with ssh client

--- cloud_install.sh
+++ cloud_install.sh
@@ -1,8 +1,8 @@
 eval `ssh-agent`
-mkdir -p /root/.ssh
-ssh-keyscan github.com >> ~/.ssh/known_hosts
+
 tar -zxvf /workspace/philippe-key.tar.gz
 mv /workspace/philippe-formation.pem  ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-add /workspace/.ssh/id_rsa
+
 ssh ec2-user@ec2-15-188-14-115.eu-west-3.compute.amazonaws.com 'bash -s' < ./docker_cloud_update.sh
