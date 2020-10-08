6ad85dce312d1f8112d23702b182a8d510dfa2a9
Philippe-Padok <philipped@padok.fr>
2020-10-08 16:21:58 +0200

    fix: update ssh ignore known host

--- cloud_install.sh
+++ cloud_install.sh
@@ -7,4 +7,4 @@ mv philippe-formation.pem  ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-add ~/.ssh/id_rsa
 
-ssh ec2-user@ec2-15-188-14-115.eu-west-3.compute.amazonaws.com 'bash -s' < ./docker_cloud_update.sh
+ssh -o "StrictHostKeyChecking no" ec2-user@ec2-15-188-14-115.eu-west-3.compute.amazonaws.com 'bash -s' < ./docker_cloud_update.sh
