0b5852e0b3e7da7b70fe80fe6bded6efa86d59a7
Térence Chateigné <terencec@padok.fr>
2021-02-10 11:44:09 +0100

    Commente, meilleurs checks, remplacement conteneur dans script
    Changement nom branche app.js
    Nommage du conteneur

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,32 @@
+#!/bin/sh
+
+# Check if the user has supplied the branch name as an argument to the script
 if [ $# = 1 ]
 then
-    if [ `git ls-remote --heads https://github.com/terencec-padok/docker-dumb-app.git $1 | grep -o $1` ]
+    branch=$1
+    # Check if the branch exists
+    branch_exists=`git ls-remote --heads git@github.com:terencec-padok/docker-dumb-app.git $branch | grep -o $branch`
+    if [ $branch_exists ] 
     then
-        echo "This branch exists"
+        # Create a specific directory for the app
         cd ~ && mkdir -p padok && cd padok
-        git clone --branch $1 https://github.com/terencec-padok/docker-dumb-app.git
-        cd docker-dumb-app
-        docker build . -t docker-dumb-app:$1
-        docker run -p 8080:8080 docker-dumb-app:$1
+        # Replace the existing directory by a fresh cloned one if it already exists
+        if [ -d "docker-dumb-app_$branch" ]
+        then
+            rm -rf docker-dumb-app_$branch
+        fi
+        git clone --quiet --branch $branch git@github.com:terencec-padok/docker-dumb-app.git docker-dumb-app_$branch
+        cd docker-dumb-app_$branch
+        # Stop and remove an existing container running the app after checking if it exists
+        container_exists=`docker ps --filter name=docker-dumb-app | grep -o docker-dumb-app:$branch`
+        if [ $container_exists ]
+        then
+            docker stop docker-dumb-app && docker rm docker-dumb-app 
+        fi
+        # Build the image
+        docker build --quiet --no-cache . -t docker-dumb-app:$branch
+        # Run the image detached with port mapping
+        docker run -d -p 8080:8080 --name docker-dumb-app docker-dumb-app:$branch
     else    
         echo "This branch does not exist"
     fi
